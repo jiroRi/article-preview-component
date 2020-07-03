caaf39f7ca4c42a5ea80e19c836e1b81b5ccabc4
Virgilio Baylosis Ricaro Jr <ricarovirgiliojr@gmail.com>
2020-07-04 07:18:33 +0800

    Added desktop popup
    Added share popup for desktop version

--- src/index.js
+++ src/index.js
@@ -1,57 +1,75 @@
-var apComponentButtonActive = document.querySelector('.ap-component__button-content--active');
-var apComponentButtonDefault = document.querySelector('.ap-component__button-content--default');
-var apComponentButtonBackground= document.querySelector('.ap-component__button--background');
+let apComponentButtonActive = document.querySelector('.ap-component__button-content--active');
+let apComponentButtonDefault = document.querySelector('.ap-component__button-content--default');
+let apComponentButtonBackground= document.querySelector('.ap-component__button--background');
+
+let apComponentButtonActiveDesktop = document.querySelector('.desktop-ap-component__button-content--active');
 
 let shareButton = document.querySelector('.ap-component__button-share');
 let shareButtonSVG = document.querySelector('.svg-arrow');
 let shareButtonBoolean = false;
 
+let windowSize = window.screen.availWidth;
+
 console.log(shareButton)
 /* console.dir(shareButton); */
 
 shareButton.addEventListener('click', function() {
-    if(!shareButtonBoolean) {
-    
-    apComponentButtonDefault.style.zIndex = 3;
-    apComponentButtonDefault.style.opacity = 0;
-    apComponentButtonDefault.style.transition = 'opacity 400ms';
 
-    apComponentButtonActive.style.opacity = 1;
-    apComponentButtonActive.style.zIndex = 4;
+    if(windowSize <= 768) {
+        if(!shareButtonBoolean) {
+        
+        apComponentButtonDefault.style.zIndex = 3;
+        apComponentButtonDefault.style.opacity = 0;
+        apComponentButtonDefault.style.transition = 'opacity 400ms';
 
-    apComponentButtonBackground.style.backgroundColor = '#48556A';
-    apComponentButtonBackground.style.width = '100%';
-/*     apComponentButtonBackgroundLeft.style.width = '84%';
-    apComponentButtonBackgroundLeft.style.left = '35%'; */
+        apComponentButtonActive.style.opacity = 1;
+        apComponentButtonActive.style.zIndex = 4;
 
-/*     shareButton.style.animation = 'buttonMovement 1s'; */
-    shareButton.style.transform = 'rotate(360deg)';
-    shareButton.style.backgroundColor = '#6D7F97';
+        apComponentButtonBackground.style.backgroundColor = '#48556A';
+        apComponentButtonBackground.style.width = '100%';
 
-    shareButtonSVG.style.fill = '#ffffff';
-    shareButtonBoolean = true;
-    console.log(shareButton)
+        shareButton.style.transform = 'rotate(360deg)';
+        shareButton.style.backgroundColor = '#6D7F97';
+
+        shareButtonSVG.style.fill = '#ffffff';
+        shareButtonBoolean = true;
+        console.log(shareButton)
+        }
+        else {
+
+        apComponentButtonDefault.style.zIndex = 4;
+        apComponentButtonDefault.style.opacity = 1;
+        apComponentButtonDefault.style.transition = 'opacity 800ms 400ms';
+
+        apComponentButtonActive.style.opacity = 0;
+        apComponentButtonActive.style.zIndex = 3;
+
+        apComponentButtonBackground.style.backgroundColor = '#48556A';
+        apComponentButtonBackground.style.width = '0';
+
+        shareButton.style.transform = 'rotate(0deg)'
+        shareButton.style.backgroundColor = '#ECF2F8';
+        
+        shareButtonSVG.style.fill = '#48556A';
+        shareButtonBoolean = false;
+        }
     }
     else {
+        if(!shareButtonBoolean) {
+            apComponentButtonActiveDesktop.style.display = 'flex';
 
-    apComponentButtonDefault.style.zIndex = 4;
-    apComponentButtonDefault.style.opacity = 1;
-    apComponentButtonDefault.style.transition = 'opacity 800ms 400ms';
+            shareButton.style.backgroundColor = '#6D7F97';
 
-    apComponentButtonActive.style.opacity = 0;
-    apComponentButtonActive.style.zIndex = 3;
+            shareButtonSVG.style.fill = '#ffffff';
+            shareButtonBoolean = true;
+        }
+        else {
+            apComponentButtonActiveDesktop.style.display = 'none'
 
-    apComponentButtonBackground.style.backgroundColor = '#48556A';
-    apComponentButtonBackground.style.width = '0';
-
-/*     shareButton.style.animation = 'buttonMovement 1s'; */
-    shareButton.style.transform = 'rotate(0deg)'
-    shareButton.style.backgroundColor = '#ECF2F8';
+            shareButton.style.backgroundColor = '#ECF2F8';
     
-    shareButtonSVG.style.fill = '#48556A';
-    shareButtonBoolean = false;
+            shareButtonSVG.style.fill = '#48556A';
+            shareButtonBoolean = false;
+        }
     }
-    setTimeout(() => {
-        
-    },1000);
 });
